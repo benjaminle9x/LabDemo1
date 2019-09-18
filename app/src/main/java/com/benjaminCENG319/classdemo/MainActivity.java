01155955e8c6d27898883ba7abc25671d5c4a3d8
benjaminle9x <54958544+benjaminle9x@users.noreply.github.com>
2019-09-18 13:52:18 -0400

    Describe what did you change...

--- app/src/main/java/com/benjaminCENG319/classdemo/MainActivity.java
+++ app/src/main/java/com/benjaminCENG319/classdemo/MainActivity.java
@@ -1,13 +1,30 @@
 package com.benjaminCENG319.classdemo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello! This is my first Android APP";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
